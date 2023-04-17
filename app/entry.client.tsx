3df64133dcc1dc057e87bd2929cd1d24ed3cd2fe
heathernfran <heathernfran@gmail.com>
2023-04-16 21:28:07 -0700

    Update LanguageDetector import

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -10,7 +10,7 @@ import { hydrateRoot } from "react-dom/client";
 import i18n from "./i18n";
 import i18next from "i18next";
 import { I18nextProvider, initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 import { getInitialNamespaces } from "remix-i18next";
 
