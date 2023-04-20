65276d0088f1e39369de0151b636dcf7c31c0005
Ben Scott <ben.scott@shopify.com>
2023-04-20 11:09:23 +0100

    Update banking translations
    - Add es translation
    - Add nesting within the namespace file

--- app/shared/banking/SharedComponent.tsx
+++ app/shared/banking/SharedComponent.tsx
@@ -2,5 +2,5 @@ import { useTranslation } from "react-i18next";
 
 export function SharedComponent() {
   const { t } = useTranslation("shared.banking");
-  return <>{t("hello")}</>;
+  return <>{t("SharedComponent.fromShared")}</>;
 }
