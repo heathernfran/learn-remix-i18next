e045fefef9b829b508e1c6c08879c335556e01c9
Ben Scott <ben.scott@shopify.com>
2023-04-20 12:34:24 +0100

    Use multiload backend on the client
    This allows multiple translations to be loaded in a single network
    request, by batching calls

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -11,19 +11,24 @@ import i18n from "./i18n";
 import i18next from "i18next";
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
+import HttpBackend from "i18next-http-backend";
+import MultiLoadBackend from "i18next-multiload-backend-adapter";
+import type { MultiloadBackendOptions } from "i18next-multiload-backend-adapter";
 import { getInitialNamespaces } from "remix-i18next";
 
 async function hydrate() {
   await i18next
     .use(initReactI18next) // Tell i18next to use the react-i18next plugin
     .use(LanguageDetector) // Setup a client-side language detector
-    .use(Backend) // Setup your backend
-    .init({
+    .use(MultiLoadBackend) // Setup your backend
+    .init<MultiloadBackendOptions>({
       ...i18n, // spread the configuration
       // This function detects the namespaces your routes rendered while SSR use
       ns: getInitialNamespaces(),
-      backend: { loadPath: "/api/i18n?lng={{lng}}&ns={{ns}}" },
+      backend: {
+        backend: HttpBackend,
+        backendOption: { loadPath: "/api/i18n?multi=1&lng={{lng}}&ns={{ns}}" },
+      },
       detection: {
         // Here only enable htmlTag detection, we'll detect the language only
         // server-side with remix-i18next, by using the `<html lang>` attribute
--- app/routes/api.i18n.ts
+++ app/routes/api.i18n.ts
@@ -9,9 +9,24 @@ const NAMESPACE_FORMAT = /^[A-Za-z0-9._-]+$/;
 
 export async function loader({ request }: LoaderArgs) {
   const searchParams = new URL(request.url).searchParams;
+  const useMultiload = searchParams.get("multi") ?? "0";
   const language = searchParams.get("lng") ?? "";
   const namespace = searchParams.get("ns") ?? "";
 
+  if (!["0", "1"].includes(useMultiload)) {
+    return errorResponse(
+      `"namespace" parameter is absent or improperly formatted. It should only contain alphanumeric characters, periods, hypens and underscores`
+    );
+  }
+
+  if (useMultiload === "1") {
+    return handleMultiLoad(language.split(" "), namespace.split(" "));
+  }
+
+  return handleSingleLoad(language, namespace);
+}
+
+async function handleSingleLoad(language: string, namespace: string) {
   if (!LANGUAGE_FORMAT.test(language)) {
     return errorResponse(
       `"lng" parameter is absent or improperly formatted. It should only contain alphanumeric characters, periods, hypens and underscores`
@@ -27,6 +42,38 @@ export async function loader({ request }: LoaderArgs) {
   if (!translations) {
     return errorResponse(
       `Could not load translations for lng "${language}" and ns "${namespace}"`
+    );
+  }
+
+  return json(translations, 200);
+}
+
+async function handleMultiLoad(languages: string[], namespaces: string[]) {
+  if (!languages.every((language) => LANGUAGE_FORMAT.test(language))) {
+    return errorResponse(
+      `"lng" parameter is absent or improperly formatted. It should only contain alphanumeric characters, periods, hypens and underscores`
+    );
+  }
+  if (!namespaces.every((namespace) => NAMESPACE_FORMAT.test(namespace))) {
+    return errorResponse(
+      `"namespace" parameter is absent or improperly formatted. It should only contain alphanumeric characters, periods, hypens and underscores`
+    );
+  }
+
+  // This shall load namespace files sequentially, it should be parallelised
+  const translations: Record<string, Record<string, object>> = {};
+  for (const language of languages) {
+    translations[language] = {};
+    for (const namespace of namespaces) {
+      translations[language][namespace] = await loadData(language, namespace);
+    }
+  }
+
+  if (!translations) {
+    return errorResponse(
+      `Could not load translations for lng "${languages.join(
+        " "
+      )}" and ns "${namespaces.join(" ")}"`
     );
   }
 
